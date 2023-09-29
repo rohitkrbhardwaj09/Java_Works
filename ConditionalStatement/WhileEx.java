9f35810bd0cf5971ae2f1c3b9253bef42e90940a
Rohit Kumar <rohit.kumar7@globallogic.com>
2023-09-29 23:35:28 +0530

    commit it

---
+++ ConditionalStatement/WhileEx.java
@@ -0,0 +1,18 @@
+package ConditionalStatement;
+class WelcomeMsg{
+
+    public void printMessage(){
+        System.out.println("Welcome All");
+    }
+}
+public class WhileEx {
+    public static void main(String[] args) {
+        WelcomeMsg wlc = new WelcomeMsg();
+
+        int i=1;
+        while (i <= 5) {
+            wlc.printMessage();
+            i++;
+        }
+    }
+}
